887b681e8a0259fcc9e66a3a2ee8192c849d0953
Rafsunjany_khan <rafsunjanykhan21@gmail.com>
2025-12-10 00:31:58 +0600

    Setup Firebase for user authentication and add firebase.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+
 
 function App() {
   return (
